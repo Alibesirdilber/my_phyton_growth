62e96da4da737220745476d70f1c21bcdf73d283
Alibesirdilber <alibesirdilber@icloud.com>
2025-12-05 17:55:24 +0300

    initial commit

---
+++ Inheritance.py
@@ -0,0 +1,27 @@
+class worker:
+    rate_of_increase = 1.1
+
+def _init_(self ,name,surname,salary)
+    self.name = name
+    self.surname = surname
+    self.salary = salary
+    self.email = name + surname + "@company.com"
+    def show_information (self) :
+        return "name: {} surname: {} salary: {} email: {}" . format(self.name,self.surname,self.salary,self.email)
+
+    worker1 = worker(" ali" ,"besir" ,5000)
+    worker2 = worker ( "ahmet", "calıskan" , 6000)
+    class developer (worker)
+        raie_of_increase = 1.2 
+        def show_information(self): 
+            return "ı am a developer"
+            
+
+    class developer ("worker") 
+    pass
+
+developer1 = developer ( "halit","ergun" , 7000)
+developer2 = developer ("fatma" "gul" 8000)
+
+print (worker1.show_information ())
+print (developer1.show_information())
