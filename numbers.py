9bf5faef8d09a8533e7ab14744d4e26e590aa6fe
Alibesirdilber <alibesirdilber@icloud.com>
2025-12-05 14:36:08 +0300

    Initial commit

---
+++ numbers.py
@@ -0,0 +1,9 @@
+# pi = 3.14159
+# pi (pi)
+
+#first_num = input ("please enter a number")
+#second_num = ("please enter another number")
+#print (float (first_num)+float(second_num))
+
+#days_in_March = 31
+#print ( str(days_in_March )+ " total days in march") 
