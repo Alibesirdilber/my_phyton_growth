1b769b9baf27b3d63eca2e7ea4e91034480df07e
Alibesirdilber <alibesirdilber@icloud.com>
2025-12-13 16:13:01 +0300

    added new content

---
+++ functions.py
@@ -0,0 +1,11 @@
+def say_hello_to(name):
+    # 'name' is the parameter
+    print(f"Hello, {name}! How are you today?")
+
+# Calling the function with an argument ('Alice')
+say_hello_to("Alice")
+# Output: Hello, Alice! How are you today?
+
+# Calling the function with a different argument ('Bob')
+say_hello_to("Bob")
+# Output: Hello, Bob! How are you today?
